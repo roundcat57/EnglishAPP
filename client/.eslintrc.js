33367958ec53edd50a99bd76dc186a0eec3f0462
roundcat57 <roundcat57@gmail.com>
2025-10-07 15:03:27 +0900

    Complete reset: Remove old project, disable ESLint completely, change project name

---
+++ client/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/rules-of-hooks': 'off'
+  }
+};
